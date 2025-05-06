3e3fd95b5c31109883257f1eb5460f0468c3cf0c
Kira Korelskaia <kiraswav@gmail.com>
2025-05-06 20:24:49 +0200

    Spring Boot project

---
+++ src/main/java/virtualhealth/model/Client.java
@@ -0,0 +1,14 @@
+package virtualhealth.model;
+
+public class Client {
+
+    private String first_name;
+
+    private String last_name;
+
+    private String email;
+
+    private String tel;
+
+    private String address;
+}
